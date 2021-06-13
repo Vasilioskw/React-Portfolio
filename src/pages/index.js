dbd1ed88283b295d3a313f732bcd0957da11d49c
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-06-12 21:52:14 -0400

    exportedNavbar and Sidebar to Pages File, then reinported them, together as Home

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
+const Home = () => {
+    return (
+        <>
+           <Sidebar />
+           <Navbar /> 
+        </>
+    )
+}
+
+export default Home;
